05cc62d41d8b4103f932ed03fa12637f7cf7a1a3
Joseph Lynn <josephlynn94@gmail.com>
2021-01-05 05:55:21 -0500

    assigned a variable to the reducers

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -3,47 +3,49 @@ import {
   SET_LOADING,
   CLEAR_USERS,
   GET_USER,
-  GET_REPOS
-} from '../types'
+  GET_REPOS,
+} from '../types';
 
-export default (state, action) => {
-  switch(action.type) {
+const githubReducer = (state, action) => {
+  switch (action.type) {
     case SEARCH_USERS:
       return {
-        // return current state 
+        // return current state
         ...state,
         // set the users data to the payload (res.data) - comign from dispatch
         users: action.payload,
-        loading: false
+        loading: false,
       };
     case GET_USER:
       return {
         // reference the current state
         ...state,
         user: action.payload,
-        loading: false 
-      }
+        loading: false,
+      };
     case CLEAR_USERS:
       return {
         ...state,
         users: [],
-        loading: false
+        loading: false,
       };
     case GET_REPOS:
       return {
         ...state,
         repos: action.payload,
-        loading: false
+        loading: false,
       };
     case SET_LOADING:
-      // want to return what is already in the state 
+      // want to return what is already in the state
       // state is immutable - can't just reassign it
       return {
-        // copies the state - and then make changes 
+        // copies the state - and then make changes
         ...state,
-        loading: true
+        loading: true,
       };
-    default: 
-      return state
+    default:
+      return state;
   }
-}
+};
+
+export default githubReducer;
